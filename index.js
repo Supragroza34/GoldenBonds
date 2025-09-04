b79d075f13449c70755464508ea98e2a484ed344
Supragroza34 <amey_tripathi@outlook.com>
2025-09-05 00:51:58 +0100

    Main ejs - Splitwise Algo

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
 
+let users = [];
+let expenses = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req,res) =>{
-    res.render("index.ejs");
+    res.render("index", {expenses});
 });
 
 
@@ -14,13 +20,28 @@ app.get("/login", (req,res) =>{
 
 app.get("/signup", (req,res) =>{
     res.render("signup.ejs");
-})
+});
 
 app.post("/submit", (req,res) =>{
     res.render("index.ejs");
-})
+});
 
+app.get("/main", (req,res) =>{
+    res.render("main.ejs");
+});
+
+app.post("/expenses", (req, res) => {
+  const { description, amount, paidBy, splitAmong } = req.body;
+  const expense = {
+    description,
+    amount: Number(amount),
+    paidBy: Number(paidBy),
+    splitAmong: splitAmong.split(",").map(Number)
+  };
+  expenses.push(expense);
+  res.redirect("/");
+});
 
 app.listen(port, () =>{
     console.log("Listening on port 3000");
-})
+});
