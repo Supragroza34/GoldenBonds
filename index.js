99187e1428b74cf523623ae5ac85fae6d260f859
Supragroza34 <amey_tripathi@outlook.com>
2025-09-05 00:35:43 +0100

    removed html - all ejs

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.get("/", (req,res) =>{
 });
 
 
+app.get("/login", (req,res) =>{
+    res.render("login.ejs");
+});
+
+app.get("/signup", (req,res) =>{
+    res.render("signup.ejs");
+})
+
+app.post("/submit", (req,res) =>{
+    res.render("index.ejs");
+})
+
+
 app.listen(port, () =>{
     console.log("Listening on port 3000");
 })
